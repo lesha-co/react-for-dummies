72d39e8b9c2751ebc7c0fed03a35fd097aa151c5
Kuzmichev Alexey <a.kuzmichev@corp.mail.ru>
2020-04-28 13:19:01 +0300

    Стартовый проект

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header class="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Привет, я Леша</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/lesha_co/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          мой инстаграм
         </a>
       </header>
     </div>
